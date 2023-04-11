0f39236ad2df4c453eaeea897f8e62ab7ae27f74
ParkJiwoon <bcp0109@gmail.com>
2023-04-12 02:26:49 +0900

    Member 도메인 추가

---
+++ src/main/java/com/example/springbootcache/model/Members.java
@@ -0,0 +1,19 @@
+package com.example.springbootcache.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@ToString
+@NoArgsConstructor
+public class Members {
+    private List<Member> members = new ArrayList<>();
+
+    public Members(List<Member> members) {
+        this.members = members;
+    }
+}
